1a767899c369088a85353c497055d17ee1bd1273
avisionh <a_vision@hotmail.co.uk>
2020-12-07 10:11:02 +0000

    test: Check that can join on text field
    This is so we can link up Camden boundary wards to crime.

--- data/README.md
+++ data/README.md
@@ -6,5 +6,6 @@ Data comes from following sources:
 
 - [On Street Crime in Camden](https://opendata.camden.gov.uk/Crime-and-Criminal-Justice/On-Street-Crime-In-Camden/qeje-7ve7)
 - [Road Collision Casualties](https://opendata.camden.gov.uk/Transport/Road-Collision-Casualties-In-Camden/puar-wf4h)
+- [Camden Ward Boundary](https://opendata.camden.gov.uk/Maps/Camden-Ward-Boundary/yqyi-6agf)
 - [Camden Street Lighting](https://opendata.camden.gov.uk/Environment/Camden-Street-Lighting/dfq3-8wzu)
 - [National Statistics Postcode Lookup UK](https://opendata.camden.gov.uk/Maps/National-Statistics-Postcode-Lookup-UK/tr8t-gqz7)
--- src/make_data/wrangle.py
+++ src/make_data/wrangle.py
@@ -1,17 +1,20 @@
 import pandas as pd
 import geopandas as gpd
-from shapely.geometry import Point
 
 
 DATA_FOLDER = 'data/raw/'
 DATA_CRIME = 'On_Street_Crime_In_Camden.csv'
-DATA_MAP = 'geo_export_e87e8b43-cf73-48e4-ad5c-692f56b45394.shp'
+DATA_MAP = 'geo_export_d270a7b6-0fb4-4fdc-9ed6-15853f2fa0d1.shp'
+DATA_LIGHTING = 'geo_export_e87e8b43-cf73-48e4-ad5c-692f56b45394.shp'
 
 df = pd.read_csv(filepath_or_buffer=DATA_FOLDER + '/' + DATA_CRIME,
                  parse_dates=['Outcome Date', 'Epoch'])
-shp_camden = gpd.read_file(filename=DATA_FOLDER + '/camden_street_lighting/' + DATA_MAP)
+shp_camden = gpd.read_file(filename=DATA_FOLDER + '/camden_ward_boundary/' + DATA_MAP)
+shp_lighting = gpd.read_file(filename=DATA_FOLDER + '/camden_street_lighting/' + DATA_LIGHTING)
 
-# create points using lat and long
-geometry = [Point(xy) for xy in zip(df['Longitude'], df['Latitude'])]
-# create GeoDataFrame to hold the Geo Series
-df_crime = gpd.GeoDataFrame(data=df, geometry=geometry)
+# join between df_crime and shp_camden on ward_name
+# check they are consistent
+crime_wards = df['Ward Name'].unique()
+boundary_wards = shp_camden['name'].unique()
+len(crime_wards) == len(boundary_wards)
+crime_wards.sort() == boundary_wards.sort()
