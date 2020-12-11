fadf851d6fa6e1b1b545211a3cb83f53b0164bd8
avisionh <a_vision@hotmail.co.uk>
2020-12-11 21:33:22 +0000

    perf: Functionalise grouping, counting and sorting
    This is so we repeat code less.

--- src/make_data/eda_crime.py
+++ src/make_data/eda_crime.py
@@ -1,3 +1,5 @@
+from src.utils.preprocess import count_crime_sort
+
 import os
 import pandas as pd
 
@@ -18,18 +20,11 @@ unique_categories = [df[x].dropna().unique() for x in categorical_variables]
 [len(x) for x in unique_categories]
 unique_categories = [sorted(x) for x in unique_categories]
 
-# grouping by Category to get crime counts
-df_category = df.value_counts(subset=['Outcome Date', 'Category']).reset_index()
-df_category = df_category.rename(columns={0: 'Crime Incidences'})
-df_category = df_category.sort_values(by=['Category', 'Outcome Date'], axis=0)
-
-# start grouping by Outcome Category
-df_outcome = df.value_counts(subset=['Outcome Date', 'Outcome Category']).reset_index()
-df_outcome = df_outcome.rename(columns={0: 'Crime Incidences'})
-df_outcome = df_outcome.sort_values(by=['Outcome Category', 'Outcome Date'], axis=0)
+# grouping by Category and Outcome Category to get crime counts
+df_category = count_crime_sort(df=df, col='Category')
+df_outcome = count_crime_sort(df=df, col='Outcome Category')
 
 # output to files for visualisation
-df_category.to_csv(path_or_buf=FOLDER_INTERIM + "/" + "df_category.csv",
-                   index=False)
-df_outcome.to_csv(path_or_buf=FOLDER_INTERIM + "/" + "df_outcome.csv",
-                  index=False)
+data_dict = {'df_category': df_category, 'df_outcome': df_outcome}
+for k, v in data_dict.items():
+    v.to_csv(path_or_buf=FOLDER_INTERIM + "/" + k + ".csv", index=False)
