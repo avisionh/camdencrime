5339ce17aa2b3cf18e2616d6ad879d6be4a15a58
avisionh <a_vision@hotmail.co.uk>
2020-12-13 17:33:23 +0000

    fix: Include population data for other years
    This is so we can get more accurate crime rates.

--- src/make_data/01_preprocess_crime.py
+++ src/make_data/01_preprocess_crime.py
@@ -12,40 +12,63 @@ DATA_POP = 'Population_20Projections_20_latest_20GLA_20set_.xlsx'
 
 # import data
 df = pd.read_csv(filepath_or_buffer=FOLDER_RAW + "/" + DATA_CRIME,
-                 parse_dates=['Outcome Date', 'Epoch'])
+                 parse_dates=["Outcome Date", "Epoch"])
 df_pop = pd.read_excel(io=FOLDER_RAW + "/" + DATA_POP,
                        sheet_name="WardP Summry",
                        header=None,
-                       names=['Ward Code', 'Ward Name', 'Population'],
-                       usecols="B,D,P",
+                       names=['Ward Code',
+                              'Ward Name',
+                              '2015',
+                              '2016',
+                              '2017',
+                              '2018',
+                              '2019',
+                              '2020'],
+                       usecols="B,D,K:P",
                        skiprows=5,
                        nrows=18)
+
+# unpivot/melt population data
+df_pop = pd.melt(frame=df_pop,
+                 id_vars=["Ward Code", "Ward Name"],
+                 var_name="Date Year",
+                 value_name="Population")
+
+# extract year
+df["Date Year"] = pd.DatetimeIndex(data=df["Outcome Date"]).year
+df["Date Month"] = pd.DatetimeIndex(data=df["Outcome Date"]).month
 
 # explore unique values of possible categorical variables
 # to confirm if they are categorical or id variables;
 # not many categories for Service nor Location Subtype
 # so not very interesting to explore further
-categorical_variables = ['Category', 'Outcome Category', 'Service', 'Location Subtype']
+categorical_variables = ["Category", "Outcome Category", "Service", "Location Subtype"]
 unique_categories = [df[x].dropna().unique() for x in categorical_variables]
 [len(x) for x in unique_categories]
 unique_categories = [sorted(x) for x in unique_categories]
 
 # join on population data
-df = df.merge(right=df_pop, how='left', on=['Ward Code', 'Ward Name'])
+df = df.dropna(subset=["Date Year"])
+df["Date Year"] = df["Date Year"].astype(int)
+df_pop["Date Year"] = df_pop["Date Year"].astype(int)
+df = df.merge(right=df_pop,
+              how='left',
+              on=["Ward Code", "Ward Name", "Date Year"],
+              validate='many_to_one')
 
 # grouping by Category and Outcome Category to get crime counts
 df_category = count_sort_crime(df=df,
-                               date_col='Outcome Date',
-                               group_cols=['Ward Name', 'Category', 'Population'])
+                               date_col=["Outcome Date", "Date Year", "Date Month"],
+                               group_cols=["Ward Name", "Category", "Population"])
 df_outcome = count_sort_crime(df=df,
-                              date_col='Outcome Date',
-                              group_cols=['Ward Name', 'Outcome Category', 'Population'])
+                              date_col=["Outcome Date", "Date Year", "Date Month"],
+                              group_cols=["Ward Name", "Outcome Category", "Population"])
 
 # get proportions for comparing across wards
-df_category['Crime Rate'] = df_category['Crime Incidences'] / df_category['Population']
-df_outcome['Crime Rate'] = df_outcome['Crime Incidences'] / df_outcome['Population']
+df_category["Crime Rate"] = df_category["Crime Incidences"] / df_category["Population"]
+df_outcome["Crime Rate"] = df_outcome["Crime Incidences"] / df_outcome["Population"]
 
 # output to files for visualisation
-data_dict = {'df_category': df_category, 'df_outcome': df_outcome}
+data_dict = {"df_category": df_category, "df_outcome": df_outcome}
 for k, v in data_dict.items():
     v.to_csv(path_or_buf=FOLDER_INTERIM + "/" + k + ".csv", index=False)
